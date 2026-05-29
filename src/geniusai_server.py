cf106409d2340014bf7d70227c707c17439c1cc6
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2026-05-29 13:25:23 +0200

    add apikey as authorized headers for CORS

--- src/geniusai_server.py
+++ src/geniusai_server.py
@@ -157,7 +157,7 @@ def _debug_file_request_payload():
 def add_cors_headers(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, apikey"
     return response
 
 
@@ -168,7 +168,7 @@ def handle_options_preflight():
         response = make_response()
         response.headers["Access-Control-Allow-Origin"] = "*"
         response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-        response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
+        response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, apikey"
         return response, 204
 
 
