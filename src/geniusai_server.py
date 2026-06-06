6e2cf687976e9f935d3c1c9101293124db337ef2
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2026-06-06 21:23:37 +0200

    Remove CORS from the app

--- src/geniusai_server.py
+++ src/geniusai_server.py
@@ -153,24 +153,6 @@ def _debug_file_request_payload():
     return payload
 
 
-@app.after_request
-def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, apikey"
-    return response
-
-
-@app.before_request
-def handle_options_preflight():
-    if request.method == "OPTIONS":
-        from flask import make_response
-        response = make_response()
-        response.headers["Access-Control-Allow-Origin"] = "*"
-        response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-        response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, apikey"
-        return response, 204
-
 
 @app.before_request
 def log_incoming_request_for_debug_file():
